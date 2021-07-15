253c6887fe3cf10c350b5e03413b4b42d62edaf0
Lucas Duarte <lucas.goes@webjump.com.br>
2021-07-15 17:01:37 -0300

    Entendendo classes e objetos

---
+++ Introducao_POO/src/Conta.php
@@ -0,0 +1,26 @@
+<?php
+
+class Conta 
+{
+    public string $cpfTitular;
+    public string $nomeTitular;
+    public float $saldo;
+
+    public function sacar(float $valorASacar) 
+    {
+        if($valorASacar > $this->saldo) {
+            echo "Saldo Indisponivel";
+        } else {
+            $this->saldo -= $valorASacar;
+        }
+    }
+
+    public function depositar(float $valorADepositar): void 
+    {
+        if($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+        } else {
+            $this->saldo += $valorADepositar;
+        }
+    }
+}
